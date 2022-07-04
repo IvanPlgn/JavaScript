ab07924b38e72b5a1e1cf03b4069779061f0ba1e
IvanPlgn <106540160+IvanPlgn@users.noreply.github.com>
2022-07-04 14:34:35 +0300

    Creat Lesson_2.js

---
+++ Class_Work/Lesson_2.js
@@ -0,0 +1,51 @@
+// console.log('hello')
+
+// // console.log(true || false || false)
+// console.log(true && true || false && true)
+
+// if (false && true){
+//     console.log('1')
+// } else {
+//     console.log('2')
+// }
+
+
+// let dog_show = true
+// let age = 18
+// let money = 2000
+// let dog = true
+
+// if (age >= 18 && money >= 1000 && dog){
+//     console.log('Get ticket')
+// } else if (age < 18){
+//     console.log('Early')
+// } else if (money < 1000){
+//     console.log('Keep calm and work hard!')
+// } else if (!dog){
+//     console.log('Get cat')
+// }
+
+// switch(dog_show) {
+//     case age >= 18:
+//         console.log('Time to win')
+// }
+
+// count = 0
+// while (count <10){
+//     // console.log(count, 'Win!!')
+//     if (count % 2 == 0){
+//         console.log(count, 'even')
+//     }
+//     count ++
+// // }
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i, 'Win!!')
+// }
+
+mm = ["Anna", "Milana", "Nina", "Ivan", "Vadim"]
+console.log (mm.length)
+
+for (let i = 0; i < mm.length; i++){
+    console.log(i, mm [i], 'Win!!')
+}
