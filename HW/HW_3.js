a1cdcb272c9c0cfeb682e3a910b75d831b03232e
IvanPlgn <106540160+IvanPlgn@users.noreply.github.com>
2022-07-04 14:52:10 +0300

    Update HW_3.js

--- HW/HW_3.js
+++ HW/HW_3.js
@@ -1,22 +1,26 @@
 // HW_1* 
 // Задания с разным количеством звездочек:)
-// 2*:
-// Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+// 3**:
+// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 
 
 const age2 = 18;
 const age3 = 60;
 
 const checkAge = function(age) {
-    age = Number(age)
-    console.log(age)
+    // age = Number(age)
+    // console.log(age)
 
-    if(age) {
+// if (typeof age == 'number') {
+    // if(age) {
+    if (age && !isNaN(age)) {
+        console.log(age)
+        
     if (age < age2) {
-        console.log('You don’t have access cause your age is' + age + 'It’s less then' + age2);
-    } else if (age >= age2 && age < age3) {
-        console.log('Welcome!');
-    } else if (age > age3) {
+        console.log('You don’t have access cause your age is '  + age + ' It’s less then ' + age2);
+    } else if (age >=  age2 && age <  age3) {
+        console.log('Welcome  !');
+    } else if (age  > age3) {
         console.log('Keep calm and look Culture channel');
     } else {
         console.log('Technical work');
@@ -28,3 +32,5 @@ checkAge(18)
 checkAge(61)
 checkAge('aaa')
 checkAge('20')
+checkAge('0')
+checkAge('')
