328d24c21a70985dda2a72bdfcfbe8f2a780315b
IvanPlgn <106540160+IvanPlgn@users.noreply.github.com>
2022-07-04 14:47:14 +0300

    Update HW_2.js

--- HW/HW_2.js
+++ HW/HW_2.js
@@ -7,22 +7,22 @@
 // }
 // Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
-
+const age = 17;
 const age2 = 18;
 const age3 = 60;
 
 const checkAge = function(age) {
-
     if (age < age2) {
-        console.log('You don’t have access cause your age is '  + age + ' It’s less then ' + age2);
+        console.log("You don’t have access cause your age is" + age + "It’s less then" + age2);
     } else if (age >=  age2 && age <  age3) {
-        console.log('Welcome  !');
+        console.log("Welcome!");
     } else if (age  > age3) {
-        console.log('Keep calm and look Culture channel');
+        console.log("Keep calm and look Culture channel");
     } else {
-        console.log('Technical work');
+        console.log("Technical work");
     }
 }
+
 checkAge(17)
 checkAge(18)
 checkAge(61)
